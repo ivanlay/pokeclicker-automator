96dcf6efc4f327892e48e74f39ead50cadb83276
Danny Lockard <danny.lockard@banno.com>
2021-07-27 13:20:02 -0500

    Update auto_hatchery.js
    Adds the ability to disable and enable hatchery without reload, and filling up the queue,

--- auto_hatchery.js
+++ auto_hatchery.js
@@ -7,15 +7,40 @@
 // @author      Ivan Lay
 // @description Automatically hatches eggs at 100% completion. Then uses the sorting and filters from the Hatcher to fill it with the best remaining Pokémon.
 // ==/UserScript==
+var hatcheryAutomationEnabled = true;
+var node = document.createElement('div');
+node.classList.add('card');
+-node.classList.add('mb-3');
+node.innerHTML = '<div id="scriptHatcheryAutomation" class="card-header"><span>Hatchery</span></div><div id="clickBody" class="card-body"><button id="toggleHatchery" class="btn btn-success" type="button">'
+               + 'Hatchery Automation Enabled</button></div>'
+
+node.setAttribute('id', 'hatcheryContainer');
+document.getElementById('left-column').appendChild(node);
+document.getElementById('toggleHatchery').addEventListener('click', ToggleHatcheryAutomation, false);
+
+function ToggleHatcheryAutomation(){
+    hatcheryAutomationEnabled = !hatcheryAutomationEnabled;
+    var button = document.getElementById('toggleHatchery');
+    if (!hatcheryAutomationEnabled) {
+        button.classList.remove('btn-success');
+        button.classList.add('btn-danger');
+        button.innerText = 'Hatchery Automation Disabled';
+    } else {
+        button.classList.remove('btn-danger');
+        button.classList.add('btn-success');
+        button.innerText = 'Hatchery Automation Enabled';
+    }
+}
 
 function loopEggs() {
   var eggLoop = setInterval(function () {
+      if (hatcheryAutomationEnabled) {
     // Attempt to hatch each egg. If the egg is at 100% it will succeed
     [0, 1, 2, 3].forEach((index) => App.game.breeding.hatchPokemonEgg(index));
 
     // Now add eggs to empty slots if we can
     while (
-      App.game.breeding.canBreedPokemon() // Helper in code to do this.
+      App.game.breeding.canBreedPokemon()  // Helper in code to do this.
     ) {
       // Filter the sorted list of Pokemon based on the parameters set in the Hatchery screen
       let filteredEggList = App.game.party.caughtPokemon.filter(
@@ -81,6 +106,7 @@ function loopEggs() {
             console.log("Added " + filteredEggList[0].name + " to the Hatchery!");
         }
     }
+  }
   }, 50); // Runs every game tick
 }
 
