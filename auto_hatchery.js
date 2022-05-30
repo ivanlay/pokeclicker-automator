f5a7e7b4f8cb0f41e139512940f14323765c2a37
lucas-pouchot <104972903+lucas-pouchot@users.noreply.github.com>
2022-05-30 13:48:50 +0200

    Update auto_hatchery.js
    really use the hatchery filter and sorter

--- auto_hatchery.js
+++ auto_hatchery.js
@@ -34,79 +34,21 @@ function ToggleHatcheryAutomation(){
 
 function loopEggs() {
   var eggLoop = setInterval(function () {
-      if (hatcheryAutomationEnabled) {
-    // Attempt to hatch each egg. If the egg is at 100% it will succeed
-    [0, 1, 2, 3].forEach((index) => App.game.breeding.hatchPokemonEgg(index));
+    if (hatcheryAutomationEnabled) {
+      // Attempt to hatch each egg. If the egg is at 100% it will succeed
+      [0, 1, 2, 3].forEach((index) => App.game.breeding.hatchPokemonEgg(index));
 
-    // Now add eggs to empty slots if we can
-    while (
-      App.game.breeding.canBreedPokemon()  // Helper in code to do this.
-    ) {
-      // Filter the sorted list of Pokemon based on the parameters set in the Hatchery screen
-      let filteredEggList = App.game.party.caughtPokemon.filter(
-        (partyPokemon) => {
-          // Only breedable Pokemon
-          if (partyPokemon.breeding || partyPokemon.level < 100) {
-            return false;
-          }
-          // Check based on category
-          if (BreedingController.filter.category() >= 0) {
-            if (
-              partyPokemon.category !== BreedingController.filter.category()
-            ) {
-              return false;
-            }
-          }
-          // Check based on shiny status
-          if (BreedingController.filter.shinyStatus() >= 0) {
-            if (
-              +partyPokemon.shiny !== BreedingController.filter.shinyStatus()
-            ) {
-              return false;
-            }
-          }
-          // Check based on native region
-          if (BreedingController.filter.region() > -2) {
-            if (
-              PokemonHelper.calcNativeRegion(partyPokemon.name) !==
-              BreedingController.filter.region()
-            ) {
-              return false;
-            }
-          }
-          // Check if either of the types match
-          const type1 =
-            BreedingController.filter.type1() > -2
-              ? BreedingController.filter.type1()
-              : null;
-          const type2 =
-            BreedingController.filter.type2() > -2
-              ? BreedingController.filter.type2()
-              : null;
-          if (type1 !== null || type2 !== null) {
-            const { type: types } = pokemonMap[partyPokemon.name];
-            if ([type1, type2].includes(PokemonType.None)) {
-              const type = type1 == PokemonType.None ? type2 : type1;
-              if (!BreedingController.isPureType(partyPokemon, type)) {
-                return false;
-              }
-            } else if (
-              (type1 !== null && !types.includes(type1)) ||
-              (type2 !== null && !types.includes(type2))
-            ) {
-              return false;
-            }
-          }
-          return true;
-        }
-      );
+      // Now add eggs to empty slots if we can
+      while (App.game.breeding.canBreedPokemon()) {  // Helper in code to do this.
+        // Filter the sorted list of Pokemon based on the parameters set in the Hatchery screen
+        let filteredEggList = PartyController.getHatcherySortedList().filter(partyPokemon => BreedingController.visible(partyPokemon)());
 
         if(App.game.breeding.canBreedPokemon()) {
-            App.game.breeding.addPokemonToHatchery(filteredEggList[0]);
-            console.log("Added " + filteredEggList[0].name + " to the Hatchery!");
+          App.game.breeding.addPokemonToHatchery(filteredEggList[0]);
+          console.log("Added " + filteredEggList[0].name + " to the Hatchery!");
         }
+      }
     }
-  }
   }, 50); // Runs every game tick
 }
 
